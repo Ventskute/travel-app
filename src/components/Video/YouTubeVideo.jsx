818df2ac7414c0926b355b554ea2409077c5d12f
Tatsiana Slapik <slapik.tatiana@gmail.com>
2021-03-07 17:32:38 +0300

    init: added youtube video

---
+++ src/components/Video/YouTubeVideo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./YouTubeVideo.scss";
+
+export default function YouTubeVideo({
+  videoId,
+  width = "560",
+  height = "315",
+}) {
+  return (
+    <iframe
+      width={width}
+      height={height}
+      src={`https://www.youtube.com/embed/${videoId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  );
+}
