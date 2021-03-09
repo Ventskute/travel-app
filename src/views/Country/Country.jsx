ad6017e9a28bb4d401244087c8594e834cffa0cb
Your Name <Anamay94.am@gmail.com>
2021-03-09 16:10:36 +0300

    feat:1st layout Country-page

--- src/views/Country/Country.jsx
+++ src/views/Country/Country.jsx
@@ -5,15 +5,46 @@ import Header from "../../components/Header/Header";
 import Map from '../../components/Map/Map';
 import data from '../../components/Map/blr.json';
 
-export default function Country() {
-  let { countryName } = useParams();
-
+function Country() {
   return (
-    <>
+    <div className="Country">
       <Header />
       <h2>Country {countryName}</h2>
+      <div className="Country-logo">
+        <div className="Country-logo__card">
+          <img className="card__image" src="./img/layout-country.jpeg" alt="layout-country"></img>
+          <h2 className="card__title">Magical Paris</h2>
+          <p className="card__subtitle">
+            City of light, one of the most famous cities in the world is waiting to be explored.
+            Some of the most famous museums, food and landmarks are waiting for you in Paris.
+          </p>
+        </div>
+        <div className="Country-logo__widgets">
+          <div className="widgets__current-wather">wather</div>
+          <div className="widgets__current-rate">rate</div>
+          <div className="widgets__current-time">time</div>
+        </div>
+      </div>
+      <div className="Country-slider">
+        <div className="Country-slider__buttons">
+          <img className="slider-button__left" src="./img/arrow_left.png" alt="" />
+          <img className="slider-right" src="./img/arrow_right.png" alt="" />
+        </div>
+        <div className="Country-slider__slides">
+          <div
+            className="Country-slider__slides_1st"
+            style={{
+              backgroundImage: `url("/dist/img/slider-img1.jpeg")`,
+            }}></div>
+          <img className="slider-button__left" src="./img/slider-img1.jpeg" alt="" />
+          <img className="slider-right" src="./img/slider-img2.jpeg" alt="" />
+          <img className="slider-right" src="./img/slider-img3.jpeg" alt="" />
+        </div>
+      </div>
       <Map data={data} />
       <Footer />
-    </>
+    </div>
   );
 }
+
+export default Country;
