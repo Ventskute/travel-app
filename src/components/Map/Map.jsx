f9cae76ba2389c2a6d9a28458f0ad5f80b50f9fc
Viktoryia Ventskute <ventskute.vika@gmail.com>
2021-03-10 19:46:46 +0300

    feat: add FullScreen option

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { YMaps, Map } from 'react-yandex-maps';
+import { YMaps, Map, FullscreenControl } from 'react-yandex-maps';
 
 const defaultValue = {
   center: [55.75, 37.57],
   zoom: 9
+}
+
+const toggleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
 }
 
 export default function CountryMap() {
@@ -11,7 +19,9 @@ export default function CountryMap() {
     <YMaps>
       <div>
         My awesome application with maps!
-        <Map defaultState={defaultValue} />
+        <Map defaultState={defaultValue}>
+          <FullscreenControl onClick={toggleFullScreen}/>
+        </Map>
       </div>
     </YMaps>
   )
