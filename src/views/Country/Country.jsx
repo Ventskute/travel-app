8660616ff20e957bfb3e8ea0092083fc36c5eb60
Andrewmurashko <anamay94.am@gmail.com>
2021-03-10 09:29:20 +0300

    feat: new layout

--- src/views/Country/Country.jsx
+++ src/views/Country/Country.jsx
@@ -4,14 +4,15 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Map from '../../components/Map/Map';
 import data from '../../components/Map/blr.json';
+import './Country.scss'
 
 function Country() {
   return (
-    <div className="Country">
+    <div className="country">
       <Header />
-      <h2>Country {countryName}</h2>
-      <div className="Country-logo">
-        <div className="Country-logo__card">
+      <h2>Country {countryName}</h2></div>
+      <div className="country-logo">
+        <div className="country-logo__card">
           <img className="card__image" src="./img/layout-country.jpeg" alt="layout-country"></img>
           <h2 className="card__title">Magical Paris</h2>
           <p className="card__subtitle">
@@ -19,26 +20,39 @@ function Country() {
             Some of the most famous museums, food and landmarks are waiting for you in Paris.
           </p>
         </div>
-        <div className="Country-logo__widgets">
+        <div className="country-logo__widgets">
           <div className="widgets__current-wather">wather</div>
           <div className="widgets__current-rate">rate</div>
           <div className="widgets__current-time">time</div>
         </div>
       </div>
-      <div className="Country-slider">
-        <div className="Country-slider__buttons">
+      <div className="country-slider">
+        <div className="country-slider__buttons">
           <img className="slider-button__left" src="./img/arrow_left.png" alt="" />
           <img className="slider-right" src="./img/arrow_right.png" alt="" />
         </div>
-        <div className="Country-slider__slides">
+        <div className="country-slider__slides">
           <div
-            className="Country-slider__slides_1st"
+            className="country-slider__slides_1st slides"
             style={{
-              backgroundImage: `url("/dist/img/slider-img1.jpeg")`,
+              backgroundImage: `url("./img/slider-img1.jpeg")`,
+            }}>
+              <div className="raiting-block">
+                <div className="raiting-block__stars">5 zvezd</div>
+                <div className="raiting-block__voted-quantity">126 ril</div>
+              </div>
+            </div>
+            <div
+            className="country-slider__slides_1st slides"
+            style={{
+              backgroundImage: `url("./img/slider-img2.jpeg")`,
             }}></div>
-          <img className="slider-button__left" src="./img/slider-img1.jpeg" alt="" />
-          <img className="slider-right" src="./img/slider-img2.jpeg" alt="" />
-          <img className="slider-right" src="./img/slider-img3.jpeg" alt="" />
+            <div
+            className="country-slider__slides_1st slides"
+            style={{
+              backgroundImage: `url("./img/slider-img3.jpeg")`,
+            }}></div>
+          
         </div>
       </div>
       <Map data={data} />
