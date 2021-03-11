729503a99d92a4b66ef11f7ac13d10f43cbbb2e6
Andrewmurashko <anamay94.am@gmail.com>
2021-03-11 15:12:30 +0300

    fix:folders names

--- src/components/country-components/widgets/widgetRate/WidgetRate.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function WidgetRate() {
-    return (
-        <div className="widgets__current-rate">rate</div>
-    )
-}
-
-export default WidgetRate
--- src/components/country-components/widgets/widgetWather/WidgetWather.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function WidgetWather() {
-    return (
-        <div className="widgets__current-wather">wather</div>
-    )
-}
-
-export default WidgetWather
