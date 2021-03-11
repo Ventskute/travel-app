11bb1726050fd3b7d535bf3bbc146f6f76d3dd62
Your Name <Anamay94.am@gmail.com>
2021-03-11 12:18:25 +0300

    feat:date widget

--- src/components/country-components/widgets/widgetTime/WidgetTime.jsx
+++ src/components/country-components/widgets/widgetTime/WidgetTime.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import './WidgetTime.scss'
+import React from 'react';
+import './WidgetTime.scss';
 
 function WidgetTime() {
-    return (
-        
-        <div className="widgets__current-time">time</div>
-    )
+  const [dateOptionsState, setDateOptionsState] = React.useState({
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone: 'America/New_York',
+    i: false,
+  });
+  React.useEffect(() => {
+    setInterval(() => {
+        setDateOptionsState({
+            ...dateOptionsState,
+            i: true,
+        })
+      }, 1000);
+  }, []);
+
+  React.useEffect(() => {
+    getCurrentDate();
+    console.log(getCurrentDate());
+  }, [dateOptionsState]);
+
+  const getCurrentDate = () => {
+    const date = new Date();
+    return date.toLocaleString('en-US', dateOptionsState);
+  };
+
+  return <div className="widgets__current-time">{getCurrentDate()}</div>;
 }
 
-export default WidgetTime
+export default WidgetTime;
