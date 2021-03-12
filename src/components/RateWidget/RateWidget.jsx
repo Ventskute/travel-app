225a3088c77511b8b62659ec3b9d99beeaaeab47
Andrewmurashko <anamay94.am@gmail.com>
2021-03-12 15:22:05 +0300

    feat:rates array, ready to use

---
+++ src/components/RateWidget/RateWidget.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+
+function RateWidget() {
+  const [rates, setRates] = React.useState([]);
+  let flag = false;
+  React.useEffect(() => {
+    fetchRates();
+  }, []);
+  React.useEffect(() => {
+    axios
+      .get(
+        'https://free.currconv.com/api/v7/convert?q=USD_BYN&compact=ultra&apiKey=c777740154b54cff282f',
+      )
+      .then(({ data }) => {
+        setRates({
+          ...rates,
+          USD_BYN: data.USD_BYN,
+        });
+      });
+  }, [rates.length]);
+  
+  const fetchRates = () => {
+    axios
+      .get(
+        'https://free.currconv.com/api/v7/convert?q=USD_EUR,USD_RUB&compact=ultra&apiKey=c777740154b54cff282f',
+      )
+      .then(({ data }) => {
+        setRates({
+          ...rates,
+          USD_EUR: data.USD_EUR,
+          USD_RUB: data.USD_RUB,
+        });
+      });
+  };
+  
+
+  return <div>{JSON.stringify(rates)}</div>;
+}
+
+export default RateWidget;
