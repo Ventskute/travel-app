61335e9ebc4d2f91011af035ac4de4f33cb15e0d
Viktoryia Ventskute <ventskute.vika@gmail.com>
2021-03-12 19:32:39 +0300

    feat: add panorama view

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,47 +1,54 @@
 import React from 'react';
-import { YMaps, Map, FullscreenControl, GeoObject, Polygon } from 'react-yandex-maps';
-import data from './blr.json';
 
-const toggleFullScreen = () => {
-  if (!document.fullscreenElement) {
-    document.documentElement.requestFullscreen();
-  } else if (document.exitFullscreen) {
-    document.exitFullscreen();
-  }
-}
+import { YMaps, Map, FullscreenControl, GeoObject, Panorama, TypeSelector } from 'react-yandex-maps';
+
+import './Map.scss';
+
+import data from './blr.json';
 
 export default function CountryMap() {
   return (
     <YMaps
       query={{
         lang: 'en_US',
+        coordorder: 'longlat',
+        apikey: "6a7ecd4e-5dac-4c29-8e69-110f766bb06c"
       }}
     >
-      <div>
-        My awesome application with maps!
-        <Map defaultState={data}>
-          <FullscreenControl onClick={toggleFullScreen}/>
-          <GeoObject
-            geometry={{
-              type: 'Point',
-              coordinates: data.center
-            }}
-            properties={{
-              iconContent: data.capital,
-            }}
-            options={{
-              preset: 'islands#blackStretchyIcon'
-            }}
-          />
-          <GeoObject
-           geometry={data.geometry}
+      <Map
+        width='100%'
+        height='100%'
+        className='map'
+        defaultState={data}
+        defaultOptions={{
+          autoFitToViewport: 'always',
+          exitFullscreenByEsc: true,
+          nativeFullscreen: true,
+        }}
+      >
+        <FullscreenControl/>
+        <GeoObject
+          geometry={{
+            type: 'Point',
+            coordinates: data.center
+          }}
+          properties={{
+            iconContent: data.capital,
+          }}
           options={{
-            fillColor: '#00FF0000',
-            strokeColor: '#0000FF',
+            preset: 'islands#blackStretchyIcon'
+          }}
+        />
+        <GeoObject
+          geometry={data.geometry}
+          options={{
+            fillColor: '#FF000011',
+            strokeColor: '#FF0000',
             strokeWidth: 5
-          }}/>
-        </Map>
-      </div>
+          }}
+        />
+        <TypeSelector />
+      </Map>
     </YMaps>
   )
 };
