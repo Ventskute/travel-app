f90e1f4c1c985707fdbba69fc674693e343c80ee
Andrewmurashko <anamay94.am@gmail.com>
2021-03-12 19:12:31 +0300

    fix:requests

--- src/views/Country/Country.jsx
+++ src/views/Country/Country.jsx
@@ -11,124 +11,23 @@ import playerBackImg from '../../assets/img/player-background.jpeg'
 import playerImg from '../../assets/img/player.jpeg'
 
 function Country() {
+  let { countryName } = useParams();
+  
   return (
     <div className="country">
       <Header />
       <h2>Country {countryName}</h2>
-      <section className="country-promo">
-        <CountryPromo />
-      </section>
-      <section className="country-slider">
-        <div className="wrapper country-slider__wrapper">
-          <Swiper />
-          {/*<img className="slider-button slider-button__left" src="./img/arrow_left.png" alt="" />
-
-          <div className="country-slider__slides">
-            <div
-              className="country-slider__slides_1st slide"
-              style={{
-                backgroundImage: `url("./img/slider-img1.jpeg")`,
-              }}>
-              <div className="raiting-block">
-                <div className="raiting-block__stars">
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star">
-                    <img src="./img/star.png" alt="" />
-                  </div>
-                </div>
-                <div className="raiting-block__voted-quantity">126 voted</div>
-              </div>
-              <h2 className="slide__title">Sea Voyages</h2>
-            </div>
-            <div
-              className="country-slider__slides_2nd slide"
-              style={{
-                backgroundImage: `url("./img/slider-img2.jpeg")`,
-              }}>
-              <div className="raiting-block">
-                <div className="raiting-block__stars">
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star">
-                    <img src="./img/star.png" alt="" />
-                  </div>
-                </div>
-                <div className="raiting-block__voted-quantity">96 voted</div>
-              </div>
-              <h2 className="slide__title">Mountain Clibs</h2>
-            </div>
-            <div
-              className="country-slider__slides_3d slide"
-              style={{
-                backgroundImage: `url("./img/slider-img3.jpeg")`,
-              }}>
-              <div className="raiting-block">
-                <div className="raiting-block__stars">
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star star-selected">
-                    <img src="./img/star-selected.png" alt="" />
-                  </div>
-                  <div className="star">
-                    <img src="./img/star.png" alt="" />
-                  </div>
-                </div>
-                <div className="raiting-block__voted-quantity">250 voted</div>
-              </div>
-              <h2 className="slide__title">Quad Rides</h2>
-            </div>
+      <div className="country">
+        <section className="country-promo">
+          <CountryPromo />
+        </section>
+        <section className="country-slider">
+          <div className="wrapper country-slider__wrapper">
+            <Swiper />
           </div>
-          <img className="slider-button slider-button__right" src="./img/arrow_right.png" alt="" />*/}
-        </div>
-      </section>
-      <section className="country-map">
-        {/* <Map /> */}
-        <div className="wrapper country-map__wrapper">
-          <img className="country-map__map" src={mapImg} alt="" />
-        </div>
-      </section>
-      <section className="country-video-block">
-        {/* <VideoPlayer /> */}
-        <div
-          className="wrapper country-video-block__wrapper"
-          style={{
-            backgroundImage: `url(${playerBackImg})`,
-          }}>
-          <div className="country-video-block__title">See Dubai from the sky</div>
-          <div className="video-player">
-            <img src={playerImg} alt="" />
-          </div>
-        </div>
-      </section>
-      <Map data={data} />
+        </section>
+        <Map data={data} />
+      </div>
       <Footer />
     </div>
   );
