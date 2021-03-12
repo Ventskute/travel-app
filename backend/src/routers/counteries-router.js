a4e174347a8efbba8256727129d88b9c92839123
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-03-12 19:05:19 +0300

    refactor: add space between middlewares

--- backend/src/routers/counteries-router.js
+++ backend/src/routers/counteries-router.js
@@ -3,9 +3,10 @@ const fs = require("fs");
 const path = require("path");
 
 const countryRouter = new Router({ prefix: "/countries" });
+const pathToData = path.resolve(__dirname, "../../data/");
 
 countryRouter.get("/", async (ctx, next) => {
-  const str = fs.createReadStream(path.resolve(__dirname, "../../countries.json"));
+  const str = fs.createReadStream(path.resolve(pathToData, "countries.json"));
   ctx.response.set("content-type", "application/json");
   ctx.body = str;
   await next();
@@ -13,16 +14,15 @@ countryRouter.get("/", async (ctx, next) => {
 
 countryRouter.get("/:ISOCode", async (ctx, next) => {
   const ISOCode = ctx.params.ISOCode;
-  const countries = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../countries.json"), "utf-8"));
+  const countries = JSON.parse(fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8"));
   const country = countries.find((country) => country.ISOCode === ISOCode);
   if (country) {
-    const attractions = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../attractions.json"), "utf-8")).filter(
+    const attractions = JSON.parse(fs.readFileSync(path.resolve(pathToData, "attractions.json"), "utf-8")).filter(
       (attraction) => attraction.countryISO === ISOCode
     );
-    const users = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../users.json"), "utf-8"));
+    const users = JSON.parse(fs.readFileSync(path.resolve(pathToData, "users.json"), "utf-8"));
     country.attractions = attractions.map((attraction) => {
       delete attraction.countryISO;
-      console.dir(attraction);
       if (attraction.ratings) {
         attraction.raitings = attraction.ratings.map((rating) => {
           rating.user = users.find((user) => user.login === rating.userLogin);
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,13 +7,17 @@ const port = 3000;
 
 const startServer = async () => {
   const app = new Koa();
+
   app.use(cors());
+
   app.use(async (ctx, next) => {
     app.use(koaSwagger({ swaggerOptions: { url: `${ctx.request.origin}/swagger` }, hideTopbar: true }));
     await next();
   });
+
   app.use(router.routes());
-  app.listen(port).on("listening", (ctx) => console.log(`wathch docs on http://localhost:${port}/docs`));
+
+  app.listen(port).on("listening", (ctx) => console.log(`watch docs on http://localhost:${port}/docs`));
 };
 
 module.exports = startServer;
