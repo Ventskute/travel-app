afdecdc842f810bf8665861d0856701522f4711f
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-03-15 07:36:46 +0300

    feat: add post rainitg endpoint

--- backend/src/routers/counteries-router.js
+++ backend/src/routers/counteries-router.js
@@ -1,9 +1,11 @@
 const Router = require("@koa/router");
+const koaBody = require("koa-body");
 const fs = require("fs");
 const path = require("path");
 
 const countryRouter = new Router({ prefix: "/countries" });
 const pathToData = path.resolve(__dirname, "../../data/");
+const pathToAttractions = path.resolve(pathToData, "attractions.json");
 
 countryRouter.get("/", async (ctx, next) => {
   const str = fs.createReadStream(path.resolve(pathToData, "countries.json"));
@@ -14,16 +16,12 @@ countryRouter.get("/", async (ctx, next) => {
 
 countryRouter.get("/:ISOCode", async (ctx, next) => {
   const ISOCode = ctx.params.ISOCode;
-  const countries = JSON.parse(
-    fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8")
-  );
+  const countries = JSON.parse(fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8"));
   const country = countries.find((country) => country.ISOCode === ISOCode);
   if (country) {
-    const attractions = JSON.parse(
-      fs.readFileSync(path.resolve(pathToData, "attractions.json"), "utf-8")
-    ).filter((attraction) => attraction.countryISO === ISOCode);
-    const users = JSON.parse(
-      fs.readFileSync(path.resolve(pathToData, "users.json"), "utf-8")
+    const users = JSON.parse(fs.readFileSync(path.resolve(pathToData, "users.json"), "utf-8"));
+    const attractions = JSON.parse(fs.readFileSync(pathToAttractions), "utf-8").filter(
+      (attraction) => attraction.countryISO === ISOCode
     );
     country.attractions = attractions.map((attraction) => {
       delete attraction.countryISO;
@@ -44,13 +42,24 @@ countryRouter.get("/:ISOCode", async (ctx, next) => {
 });
 
 countryRouter.get("/countryoftheday", async (ctx, next) => {
-  const countries = JSON.parse(
-    fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8")
-  );
+  const countries = JSON.parse(fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8"));
   const country = countries.find((country) => country.ISOCode === "BY");
   ctx.response.set("content-type", "application/json");
   ctx.body = country;
   ctx.status = 200;
+  await next();
+});
+countryRouter.post("/:ISOCode/:attractionId", koaBody(), async (ctx, next) => {
+  const params = ctx.request.body;
+  const { attractionId, ISOCode } = ctx.params;
+  const attractions = JSON.parse(fs.readFileSync(pathToAttractions), "utf-8");
+  console.log(attractions);
+  const attraction = attractions.find((attraction) => attraction.id === attractionId);
+  attraction.ratings.push(params);
+  attraction.rating = Math.floor(
+    attraction.ratings.reduce((acc, rait) => acc + rait.score, 0) / attraction.ratings.length
+  );
+  fs.writeFileSync(pathToAttractions, JSON.stringify(attractions));
   await next();
 });
 
