bc6ed984fe73785156d53e15debeb01409443b7b
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-03-15 07:34:22 +0300

    feat: add signup and signin endpoints

---
+++ backend/src/routers/auth-router.js
@@ -0,0 +1,41 @@
+const Router = require("@koa/router");
+const koaBody = require("koa-body");
+const fs = require("fs");
+const path = require("path");
+
+const authRouter = new Router();
+
+const pathToUsers = path.resolve(__dirname, "../../data/users.json");
+
+authRouter.post("/signup", koaBody({ multipart: true }), async (ctx, next) => {
+  const { login, password, name } = ctx.request.body;
+  const users = JSON.parse(fs.readFileSync(pathToUsers));
+
+  if (users.every((user) => user.login !== login)) {
+    let pathToAvatar = null;
+    if (ctx.request.files?.avatar?.type.includes("image")) {
+      pathToAvatar = `statics/users/${login}${ctx.request.files.avatar.name.replace(/^.+[.]/, ".")}`;
+      const avatarStrR = fs.createReadStream(ctx.request.files.avatar.path);
+      const avatarStrW = fs.createWriteStream(`data/${pathToAvatar}`);
+      avatarStrR.pipe(avatarStrW);
+    }
+    const newUser = { login, password, name, avatar: pathToAvatar };
+    users.push(newUser);
+    fs.writeFileSync(pathToUsers, JSON.stringify(users));
+    ctx.status = 200;
+  } else {
+    ctx.status = 403;
+  }
+
+  await next();
+});
+
+authRouter.post("/signin", koaBody({ multipart: true }), async (ctx, next) => {
+  const { login, password } = ctx.request.body;
+  const users = JSON.parse(fs.readFileSync(pathToUsers));
+  const user = users.find((user) => user.login === login);
+  ctx.status = user?.password === password ? 200 : 403;
+  await next();
+});
+
+module.exports = authRouter.routes();
