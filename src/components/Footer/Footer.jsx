091f2c1f1de9cb9a5a7d44fc6e71528d1a91a6c6
Tatsiana Slapik <slapik.tatiana@gmail.com>
2021-03-16 12:49:13 +0300

    feat: added links and icons to the footer, added a logo to the header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,44 @@
 import React from "react";
-
+import { Img } from "react-image";
+import GitHub from "../../assets/logo/GitHub.png";
 import "./Footer.scss";
+const GitHubLogo = () => <Img src={GitHub} width="18px" height="18px" />;
 
 export default function Footer() {
   return (
     <>
       <footer>
-        <div className="container">
-          <h4>2021</h4>
+        <div className="container_footer">
+          <p>
+            Viktoryia Ventskute
+            <a href="https://github.com/Ventskute">
+              <GitHubLogo />
+            </a>
+          </p>
+
+          <p>
+            Andrew Murashko
+            <a href="https://github.com/Andrewmurashko">
+              <GitHubLogo />
+            </a>{" "}
+          </p>
+          <p>
+            Aleksei
+            <a href="https://github.com/AlekseiBY">
+              <GitHubLogo />
+            </a>{" "}
+          </p>
+          <p>
+            Tatsiana Slapik
+            <a href="https://github.com/TatsianaSlapik">
+              <GitHubLogo />
+            </a>
+          </p>
+          <p>
+            <a class="rss" href="https://rs.school/react/">
+              <span class="rss-year">'21</span>
+            </a>
+          </p>
         </div>
       </footer>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { Img } from "react-image";
+import TravelLogo from "../../assets/logo/travel_app.png";
 import "./Header.scss";
+const TravelAppLogo = () => <Img src={TravelLogo} width="40px" height="40px" />;
 
 export default function Header() {
   return (
     <>
       <header>
         <div className="container">
-          <Link to="/">Main</Link>
+          <Link to="/">
+            <TravelAppLogo />
+          </Link>
         </div>
       </header>
     </>
