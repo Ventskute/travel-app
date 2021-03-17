16e2f8ebeb502f0f58f4d068070ba4bc4061b4e4
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-03-17 20:07:16 +0300

    feat: add jest modules

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,6 +26,7 @@ export default function Footer() {
   return (
     <>
       <footer>
+<<<<<<< HEAD
         <div className="container">
           <ul>
           {
@@ -42,6 +43,39 @@ export default function Footer() {
           <a className="rss" href="https://rs.school/react/" target='_blank'>
             <span className="rss-year">'21</span>
           </a>
+=======
+        <div className="container_footer">
+          <p>
+            Viktoryia Ventskute
+            <a href="https://github.com/Ventskute">
+              <img className="github_logo" src={GitHub} />
+            </a>
+          </p>
+
+          <p>
+            Andrew Murashko
+            <a href="https://github.com/Andrewmurashko">
+              <img className="github_logo" src={GitHub} />
+            </a>{" "}
+          </p>
+          <p>
+            Aleksei
+            <a href="https://github.com/AlekseiBY">
+              <img className="github_logo" src={GitHub} />
+            </a>{" "}
+          </p>
+          <p>
+            Tatsiana Slapik
+            <a href="https://github.com/TatsianaSlapik">
+              <img className="github_logo" src={GitHub} />
+            </a>
+          </p>
+          <p>
+            <a className="rss" href="https://rs.school/react/">
+              <span className="rss-year">'21</span>
+            </a>
+          </p>
+>>>>>>> 8f4a8ba (feat: add jest modules)
         </div>
       </footer>
     </>
