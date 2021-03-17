abee0eab69808fa55961d2c79a01d71d81456bf4
TatsianaSlapik <70775751+TatsianaSlapik@users.noreply.github.com>
2021-03-17 17:47:42 +0300

    feat: added links and icons to the footer, added a logo to the header (#13)
    * feat: added links and icons to the footer, added a logo to the header
    
    * feat: PR review changes
    
    * feat: added logo and button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import GitHub from "../../assets/logo/GitHub.png";
 
 import "./Footer.scss";
 
@@ -6,8 +7,37 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <div className="container">
-          <h4>2021</h4>
+        <div className="container_footer">
+          <p>
+            Viktoryia Ventskute
+            <a href="https://github.com/Ventskute">
+              <img className="github_logo" src={GitHub} />
+            </a>
+          </p>
+
+          <p>
+            Andrew Murashko
+            <a href="https://github.com/Andrewmurashko">
+              <img className="github_logo" src={GitHub} />
+            </a>{" "}
+          </p>
+          <p>
+            Aleksei
+            <a href="https://github.com/AlekseiBY">
+              <img className="github_logo" src={GitHub} />
+            </a>{" "}
+          </p>
+          <p>
+            Tatsiana Slapik
+            <a href="https://github.com/TatsianaSlapik">
+              <img className="github_logo" src={GitHub} />
+            </a>
+          </p>
+          <p>
+            <a class="rss" href="https://rs.school/react/">
+              <span class="rss-year">'21</span>
+            </a>
+          </p>
         </div>
       </footer>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "./Header.scss";
 
 export default function Header() {
+  useEffect(() => {
+    document.addEventListener("mousemove", function (e) {
+      let mouseX = e.pageX;
+      let mouseY = e.pageY;
+      let traX = (4 * mouseX) / 570 + 40;
+      let traY = (4 * mouseY) / 570 + 50;
+      let logoEl = document.getElementsByClassName("logo_title")[0];
+      logoEl.style.cssText = `background-position: ${traX}% ${traY}%`;
+    });
+  }, []);
+
+  let userLogged;
+
   return (
     <>
       <header>
         <div className="container">
-          <Link to="/">Main</Link>
+          <div className="logo_title">
+            <Link to="/">
+              <p>TRAVEL APP</p>
+            </Link>
+            <button className="sign">Sign in</button>
+          </div>
         </div>
       </header>
     </>
