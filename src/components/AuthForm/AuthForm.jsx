4cad7d7ed3852cd93845a9fd25b94586c70e33a6
Viktoryia Ventskute <45541398+Ventskute@users.noreply.github.com>
2021-03-17 23:37:03 +0300

    Login modal (#22)
    * feat: add login styles
    
    * feat: add image and name of user
    
    * fix: footer names
    
    * fix: hover color in footer

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,15 +1,20 @@
 import React, { useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import { signin, signup } from "../../utils/api";
 
 import "./AuthForm.scss";
 
 const AuthForm = ({ isSignup, setUser, closeForm }) => {
+  const { dict } = useSelector(state => state);
+
   const [imgUrl, setImgUrl] = useState(
     "https://www.pinclipart.com/picdir/big/15-154296_gender-neutral-user-account-icon-png-clipart.png"
   );
   const [loginValue, setLoginValue] = useState("");
   const [loginPlaceHolder, setLoginPlaceHolder] = useState("name");
+
   const form = useRef(null);
+
   const reader = new FileReader();
   reader.onload = (e) => {
     setImgUrl(e.target.result);
@@ -18,41 +23,48 @@ const AuthForm = ({ isSignup, setUser, closeForm }) => {
   const submit = async (e) => {
     e.preventDefault();
     const data = new FormData(form.current);
-    const resStatus = isSignup ? await signup(data) : await signin(data);
+    const res = isSignup ? await signup(data) : await signin(data);
+    const resStatus = await res.status;
+    const resUser = await res.user;
+
     if (resStatus === 403) {
       setLoginValue("");
       isSignup
         ? setLoginPlaceHolder("this name already taken")
         : setLoginPlaceHolder("name or password is incorrect");
     } else if (resStatus === 200) {
-      setUser({ login: loginValue });
+      if (resUser) {
+        setUser({
+          login: loginValue,
+          avatar: `http://localhost:3000/${resUser.avatar}` });
+      } else {
+        setUser({
+          login: loginValue,
+          avatar: `http://localhost:3000/statics/users/${loginValue.toLowerCase()}${imgUrl.substr(imgUrl.lastIndexOf("."))}` });
+      }
       closeForm();
     }
   };
 
   return (
-    <div className="auth-form-container">
-      <h3>{isSignup ? "Signup" : "Signin"} form</h3>
-      <form className="auth-form" ref={form} onSubmit={submit}>
-        <div className="auth-form_input">
+    <div className="auth-modal">
+      <div className="auth-form-container">
+        <h2 className="auth-form--title">{isSignup ? "Signup" : "Signin"} form</h2>
+        <form className="auth-form" ref={form} onSubmit={submit}>
           <input
             name="login"
             type="text"
             required
             placeholder={loginPlaceHolder}
             value={loginValue}
+            className="auth-form--input"
             onChange={(e) => {
               setLoginValue(e.target.value);
             }}
           />
-        </div>
-        <div className="auth-form_input">
-          <input type="password" name="password" required placeholder="password" />
-        </div>
-        {isSignup && (
-          <div className="auth-form_input input-imgFile">
-            <label className="add-avatar-btn">
-              <img src={imgUrl} className="preImg" alt="avatar" />
+          <input type="password" name="password" className="auth-form--input" required placeholder="password" />
+          {isSignup && (
+            <label className="add-avatar-btn auth-form--input_file" style={{backgroundImage: `url(${imgUrl})`}}>
               <input
                 type="file"
                 accept="image/*"
@@ -61,13 +73,14 @@ const AuthForm = ({ isSignup, setUser, closeForm }) => {
                   reader.readAsDataURL(e.target.files[0]);
                 }}
               />
-              add avatar
+              {dict.ADD_AVATAR}
             </label>
-          </div>
-        )}
-        <input type="submit" className="auth-form_submit button" value="submit" />
-      </form>
+          )}
+          <input type="submit" className="auth-form_submit" value="submit" />
+        </form>
+      </div>
     </div>
+
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import actions from "../../utils/actions";
 
 import "./Header.scss";
 
 export default function Header({ children }) {
+  const { user } = useSelector(state => state);
+  const dispatch = useDispatch();
+
   const logo = useRef(null);
+
+  const logoutUser = () => {
+    dispatch({ type: actions.REMOVE_USER });
+  };
+  const openSignupForm = () => {
+    dispatch({
+      type: actions.SET_AUTHFORM,
+      payload: { isFormOpen: true, isSignup: true }
+    });
+  };
+  const openSigninForm = () => {
+    dispatch({
+      type: actions.SET_AUTHFORM,
+      payload: { isFormOpen: true, isSignup: false }
+    });
+  };
 
   useEffect(() => {
     document.addEventListener("mousemove", function (e) {
@@ -16,19 +37,31 @@ export default function Header({ children }) {
     });
   }, []);
 
-  let userLogged;
-
   return (
-    <>
       <header>
         <div className="container">
           <Link to="/">
             <p className="logo_title" ref={logo}>TRAVEL APP</p>
           </Link>
-          <button className="sign">Sign in</button>
+
+          <div className="login-buttons">
+            { !user && <>
+              <button className="login-button" onClick={openSignupForm}>Sign up</button>
+              <button className="login-button" onClick={openSigninForm}>Log in</button>
+            </>}
+            { user && <>
+              { user.avatar &&
+                <div className="avatar-wrapper">
+                  <img src={user.avatar}/>
+                </div>
+              }
+              <h3 className="login-name">{user.login}</h3>
+              <button className="login-button" onClick={logoutUser}>Log out</button>
+            </>}
+          </div>
+
           {children && children}
         </div>
       </header>
-    </>
   );
 }
