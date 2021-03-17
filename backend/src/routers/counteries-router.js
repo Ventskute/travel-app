2a7fd33981d1a55e950ef99f039ad95551ca25d9
AlekseiBY <aleksei.kupchinskii@gmail.com>
2021-03-18 01:14:56 +0300

    Discr lang (#21)
    * fix: new package-lock
    
    * fix: fix bug in county-router
    
    * feat: add jest modules
    
    * feat: add languages to countries
    
    * feat: merge translates
    
    Co-authored-by: Mikita Liaukovich <Mikita_Liaukovich@epam.com>

--- backend/src/routers/counteries-router.js
+++ backend/src/routers/counteries-router.js
@@ -7,34 +7,71 @@ const countryRouter = new Router({ prefix: "/countries" });
 const pathToData = path.resolve(__dirname, "../../data/");
 const pathToAttractions = path.resolve(pathToData, "attractions.json");
 
+const parseCountry = (country, lang) => {
+  const { name, capitalName, description } = country.lang[lang];
+  const capitalCoordinates = country.capitalCoordinates;
+  delete country.capitalCoordinates;
+  delete country.lang;
+  country.name = name;
+  country.capital = {
+    name: capitalName,
+    coordinates: capitalCoordinates,
+  };
+  country.description = description;
+  return country;
+};
+
 countryRouter.get("/", async (ctx, next) => {
-  const str = fs.createReadStream(path.resolve(pathToData, "countries.json"));
+  const { lang = 'ru_RU' } = ctx.query;
+  const countries = JSON.parse(
+    fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8")
+  ).map((country) => {
+    const parsedCountry = parseCountry(country, lang);
+    delete parsedCountry.description;
+    return parsedCountry;
+  });
+
   ctx.response.set("content-type", "application/json");
-  ctx.body = str;
+  ctx.body = JSON.stringify(countries);
   await next();
 });
 
 countryRouter.get("/:ISOCode", async (ctx, next) => {
   const ISOCode = ctx.params.ISOCode;
-  const countries = JSON.parse(fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8"));
+  const { lang = 'ru_RU' } = ctx.query;
+  const countries = JSON.parse(
+    fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8")
+  );
   const country = countries.find((country) => country.ISOCode === ISOCode);
   if (country) {
     const users = JSON.parse(fs.readFileSync(path.resolve(pathToData, "users.json"), "utf-8"));
     const attractions = JSON.parse(fs.readFileSync(pathToAttractions), "utf-8").filter(
       (attraction) => attraction.countryISO === ISOCode
     );
-    country.attractions = attractions.map((attraction) => {
+
+    const parsedCountry = parseCountry(country, lang);
+    parsedCountry.attractions = attractions.map((attraction) => {
+      const name = attraction.lang[lang].name;
+      const description = attraction.lang[lang].description;
+
       delete attraction.countryISO;
+      delete attraction.lang;
+
+      attraction.name = name;
+      attraction.description = description;
+
       if (attraction.ratings) {
-        attraction.raitings = attraction.ratings.map((rating) => {
+        attraction.ratings = attraction.ratings.map((rating) => {
           rating.user = users.find((user) => user.login === rating.userLogin);
+          rating.user && delete rating.user.password;
           delete rating.userLogin;
           return rating;
         });
       }
       return attraction;
     });
-    ctx.body = country;
+
+    ctx.body = parsedCountry;
   } else {
     ctx.status = 404;
   }
@@ -42,17 +79,21 @@ countryRouter.get("/:ISOCode", async (ctx, next) => {
 });
 
 countryRouter.get("/countryoftheday", async (ctx, next) => {
-  const countries = JSON.parse(fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8"));
-  const country = countries[new Date().getDate() % countries.length];
+  const { lang = 'ru_RU' } = ctx.query;
+  const countries = JSON.parse(
+    fs.readFileSync(path.resolve(pathToData, "countries.json"), "utf-8")
+  );
+  const country = countries[new Date().getDate() % countries.length]
+
   ctx.response.set("content-type", "application/json");
-  ctx.body = country;
+  ctx.body = lang ? parseCountry(country, lang) : parseCountry(country, "ru_RU");
   ctx.status = 200;
   await next();
 });
 
 countryRouter.post("/:ISOCode/:attractionId", koaBody(), async (ctx, next) => {
   const params = ctx.query;
-  console.dir(param);
+
   const { attractionId, ISOCode } = ctx.params;
   const attractions = JSON.parse(fs.readFileSync(pathToAttractions), "utf-8");
   const attraction = attractions.find((attraction) => attraction.id === attractionId);
