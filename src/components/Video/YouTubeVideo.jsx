eb7409f9b862c4791b7e3887e0ecd77b2e2ceaee
Tatsiana Slapik <slapik.tatiana@gmail.com>
2021-03-18 15:17:57 +0300

    fix: video

--- src/components/Video/YouTubeVideo.jsx
+++ src/components/Video/YouTubeVideo.jsx
@@ -10,7 +10,7 @@ export default function YouTubeVideo({
     <iframe
       width={width}
       height={height}
-      src={`https://www.youtube.com/embed/${videoId}`}
+      src={`${videoId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
