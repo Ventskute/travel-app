5c3b45823638b7ea5365d4f4004e691b5127c3a4
TatsianaSlapik <70775751+TatsianaSlapik@users.noreply.github.com>
2021-03-18 15:31:13 +0300

    fix: video (#25)

--- src/views/Country/Country.jsx
+++ src/views/Country/Country.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import Footer from '../../components/Footer/Footer';
-import Header from '../../components/Header/Header';
-import Map from '../../components/Map/Map';
-import CountryPromo from '../../components/CountryPromo/CountryPromo';
-import GeneralAttractions from '../../components/GeneralAttractions/GeneralAttractions';
-import YouTubeVideo from '../../components/Video/YouTubeVideo';
+import React from "react";
+import { useParams } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Map from "../../components/Map/Map";
+import CountryPromo from "../../components/CountryPromo/CountryPromo";
+import GeneralAttractions from "../../components/GeneralAttractions/GeneralAttractions";
+import YouTubeVideo from "../../components/Video/YouTubeVideo";
 
-import data from '../../components/Map/blr.json';
+import data from "../../components/Map/blr.json";
 
-import './Country.scss';
-import { getCountry } from '../../utils/api';
-import { useSelector } from 'react-redux';
+import "./Country.scss";
+import { getCountry } from "../../utils/api";
+import { useSelector } from "react-redux";
 
 function Country() {
-  const { locale } = useSelector(state => state);
+  const { locale } = useSelector((state) => state);
   const [countryState, setCountryState] = React.useState(null);
 
   let { ISOCode } = useParams();
 
   React.useEffect(() => {
-    getCountry(locale, ISOCode)
-      .then((res) => setCountryState(res))
+    getCountry(locale, ISOCode).then((res) => setCountryState(res));
   }, []);
 
   return (
@@ -35,9 +34,12 @@ function Country() {
           </section>
           <Map data={data} />
           <section className="video">
-            <div className="video-wrapper container" style={{ backgroundImage: `url(${countryState.promo})` }}>
+            <div
+              className="video-wrapper container"
+              style={{ backgroundImage: `url(${countryState.promo})` }}
+            >
               <h2 className="video__title">{countryState.name}</h2>
-              <YouTubeVideo />
+              <YouTubeVideo videoId={countryState.videoUrl} />
             </div>
           </section>
         </React.Fragment>
